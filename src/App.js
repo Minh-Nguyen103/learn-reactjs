3ec9b0f3a01f6d0464590eb29817fd7a7ea554e7
Minh Nguyen <pmnguyen1032000@gmail.com>
2022-03-03 06:32:23 +0700

    Install react-router-dom Create routing for each feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import "./App.css";
 import React from "react";
+import { Route } from "react-router-dom";
+import SongFeature from "./features/Song";
 import TodoFeature from "./features/Todo";
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      Header
+
+      <Route path= '/todos' component = {TodoFeature}/>
+      <Route path= '/albums' component = {SongFeature}/>
+
     </div>
   );
 }
