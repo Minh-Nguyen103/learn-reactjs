a3ba08dca5451554bc71c2d75408c14fc39f0d3d
Minh Nguyen <pmnguyen1032000@gmail.com>
2022-03-03 08:25:03 +0700

    Nested routing

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import TodoList from './components/TodoList';
+import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import ListPage from './pages/ListPage';
+import DetailPage from './pages/DetailPage';
 
 
 TodoFeature.propTypes = {
@@ -8,66 +9,16 @@ TodoFeature.propTypes = {
 };
 
 function TodoFeature(props) {
-
-    const initTodoList = [
-        {
-            id: 1,
-            title: 'JS',
-            status: 'new',
-        },
-
-        {
-            id: 2,
-            title: 'ReactJS',
-            status: 'completed',
-        },
-
-        {
-            id: 3,
-            title: 'CSS',
-            status: 'new',
-        },
-    ]
-
-    const [todoList, setTodoList] = useState(initTodoList);
-    const [filteredStatus, setFilteredStatus] = useState('all');
-
-
-    const handleTodoClick = (todo, idx) => {
-        console.log(todo);
-        const newTodoList = [...todoList];
-
-        newTodoList[idx] = {
-            ...newTodoList[idx],
-            status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
-        };
-
-        setTodoList(newTodoList)
-
-    }
-
-    const handleShowAllClick = () => {
-        setFilteredStatus('all');
-    };
-    const handlleShowCompletedClick = () => {
-
-        setFilteredStatus('completed');
-    };
-    const handleShowNewClick = () => {
-        setFilteredStatus('new');
-    };
-
-    const renderedTodoList = todoList.filter(todo => filteredStatus === 'all' || todo.status === filteredStatus);
-
-
+    const match = useRouteMatch();
 
     return (
         <div>
-            <h3>TODOLIST</h3>
-            <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
-            <button onClick={handleShowAllClick}>Show All</button>
-            <button onClick={handlleShowCompletedClick}>Show Completed</button>
-            <button onClick={handleShowNewClick}>Show New </button>
+            TODO SHARE ID
+
+            <Switch>
+                <Route path={match.path} component={ListPage} exact/>
+                <Route path={`${match.path}/:todoId`} component={DetailPage}/>
+            </Switch>
         </div>
     );
 }
---
+++ src/features/Todo/pages/ListPage/index.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import TodoList from '../../components/TodoList';
+
+
+ListPage.propTypes = {
+
+};
+
+function ListPage(props) {
+
+    const initTodoList = [
+        {
+            id: 1,
+            title: 'JS',
+            status: 'new',
+        },
+
+        {
+            id: 2,
+            title: 'ReactJS',
+            status: 'completed',
+        },
+
+        {
+            id: 3,
+            title: 'CSS',
+            status: 'new',
+        },
+    ]
+
+    const [todoList, setTodoList] = useState(initTodoList);
+    const [filteredStatus, setFilteredStatus] = useState('all');
+
+
+    const handleTodoClick = (todo, idx) => {
+        console.log(todo);
+        const newTodoList = [...todoList];
+
+        newTodoList[idx] = {
+            ...newTodoList[idx],
+            status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
+        };
+
+        setTodoList(newTodoList)
+
+    }
+
+    const handleShowAllClick = () => {
+        setFilteredStatus('all');
+    };
+    const handlleShowCompletedClick = () => {
+
+        setFilteredStatus('completed');
+    };
+    const handleShowNewClick = () => {
+        setFilteredStatus('new');
+    };
+
+    const renderedTodoList = todoList.filter(todo => filteredStatus === 'all' || todo.status === filteredStatus);
+
+
+
+    return (
+        <div>
+            <h3>TODOLIST</h3>
+            <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
+            <button onClick={handleShowAllClick}>Show All</button>
+            <button onClick={handlleShowCompletedClick}>Show Completed</button>
+            <button onClick={handleShowNewClick}>Show New </button>
+        </div>
+    );
+}
+
+export default ListPage;
