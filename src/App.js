1a2665669959dd37bba07d88032137ebb4ebfce8
Minh Nguyen <pmnguyen1032000@gmail.com>
2022-03-11 18:53:43 +0700

    Add app bar UI

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 // import "./App.css";
 import React from 'react';
-import { Link, NavLink, Redirect, Route, Switch } from 'react-router-dom';
-import SongFeature from './features/Song';
-import TodoFeature from './features/Todo';
+import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 import NotFound from './components/NotFound';
 import CounterFeature from './features/Counter';
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  text-align: center;
-  font-weight: bold;
-
-  color: ${(props) => props.color || 'green'};
-`;
+import SongFeature from './features/Song';
+import TodoFeature from './features/Todo';
 
 function App() {
   return (
     <div className="App">
-      <Title color="goldenrod">HEADING</Title>
-      <p>
-        <NavLink to="/todos" activeClassName="active-menu">
-          Todos
-        </NavLink>
-      </p>
-      <p>
-        <NavLink to="/albums">Albums</NavLink>
-      </p>
-      <p>
-        <NavLink to="/counter">Counter</NavLink>
-      </p>
+      <Header />
+
       <Switch>
         <Redirect from="/home" to="/" exact />
         <Redirect from="/post-list/:postId" to="/post/:postId" exact />
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import './styles.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import FaceIcon from '@material-ui/icons/Face';
 
-Header.propTypes = {
-    
-};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-function Header(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <FaceIcon className={classes.menuButton} />
+
+          <Typography variant="h6" className={classes.title}>
+            Minh Nguyên Shop
+          </Typography>
+
+          <Button color="inherit">Todos</Button>
+          <Button color="inherit">Albums</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-export default Header;
