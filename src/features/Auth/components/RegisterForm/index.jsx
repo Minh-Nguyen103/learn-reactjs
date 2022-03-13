c1042ece37a7bcac6103861d37c944afca9b7a1d
Minh Nguyen <pmnguyen1032000@gmail.com>
2022-03-13 17:37:20 +0700

    Styles for register form

--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import InputField from '../../../../components/Form-controls/InputField';
-import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { Avatar, Typography } from '@material-ui/core';
+import { Avatar, Button, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { LockOutlined } from '@material-ui/icons';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import InputField from '../../../../components/Form-controls/InputField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(4),
+  },
+
+  avatar: {
+    margin: '0 auto',
+    backgroundColor: theme.palette.secondary.main,
+  },
+
+  title: {
+    margin: theme.spacing(2, 0, 3, 0),
+    textAlign: 'center',
+  },
+
+  submit: {
+    margin: theme.spacing(3, 0, 2, 0),
+  },
+}));
 
 RegisterForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
 function RegisterForm(props) {
+  const classes = useStyles();
+
   const schema = yup.object().shape({
     title: yup.string().required('Please enter title'),
   });
@@ -35,13 +58,13 @@ function RegisterForm(props) {
     form.reset();
   };
   return (
-    <div>
-      <Avatar>
+    <div className={classes.root}>
+      <Avatar className={classes.avatar}>
         <LockOutlined></LockOutlined>
       </Avatar>
 
-      <Typography component={'h3'} variant={'h5'}>
-        CREATE AN ACCOUNT
+      <Typography className={classes.title} component={'h3'} variant={'h5'}>
+        Create An Account
       </Typography>
 
       <form onSubmit={form.handleSubmit(handleSubmit)}>
@@ -49,6 +72,10 @@ function RegisterForm(props) {
         <InputField name="email" label="Email" form={form} />
         <InputField name="password" label="Password" form={form} />
         <InputField name="retypePassword" label="Retype Password" form={form} />
+
+        <Button className={classes.submit} color="primary" variant="contained" fullWidth>
+          Create an account
+        </Button>
       </form>
     </div>
   );
