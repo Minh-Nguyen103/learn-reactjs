387d7f9e31c5145f877d6e756e61cfb94f1d2aeb
Minh Nguyen <pmnguyen1032000@gmail.com>
2022-03-14 13:45:29 +0700

    Create password field

---
+++ src/components/Form-controls/PasswordField/index.jsx
@@ -0,0 +1,64 @@
+import { TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Controller } from 'react-hook-form';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import FilledInput from '@material-ui/core/FilledInput';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+PasswordField.propTypes = {
+  form: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+function PasswordField(props) {
+  const { form, name, label, disabled } = props;
+  const { errors, formState } = form;
+  const hasError = errors[name] && formState.touched[name];
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword((x) => !x);
+  };
+
+  return (
+    <div>
+      <FormControl fullWidth margin="normal" variant="outlined">
+        <InputLabel htmlFor={name}>{label}</InputLabel>
+        <Controller
+          name={name}
+          control={form.control}
+          as={OutlinedInput}
+          id={name}
+          label={label}
+          type={showPassword ? 'text' : 'password'}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton aria-label="toggle password visibility" onClick={toggleShowPassword}>
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+          disabled={disabled}
+          error={hasError}
+          helperText={errors[name]?.message}
+        />
+      </FormControl>
+    </div>
+  );
+}
+
+export default PasswordField;
